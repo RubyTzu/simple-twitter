725f1afb0809253158c9f33fc4603846d0af0ab4
Kaijun <kaijun_huang@outlook.com>
2023-06-13 11:07:46 +0800

    Modify: fix bug

--- src/api/admin.js
+++ src/api/admin.js
@@ -2,21 +2,7 @@ import axios from "axios";
 const baseUrl = "https://twitter-2023.herokuapp.com";
 const token = localStorage.getItem("authToken");
 
-// // export const adminLogin = async ({ account, password }) => {
-// //   const { data } = await axios.post(`${baseUrl}/api/signin`, {
-// //     account,
-// //     password,
-// //   });
-// //   const authToken = data.token;
-// //   console.log(data.data.user.role);
-// //   if (authToken) {
-// //     return { success: true, ...data };
-// //   } else {
-// //     return { success: false, ...data };
-// //   }
-// // };
-
-export const login = async ({ account, password }) => {
+export const adminLogin = async ({ account, password }) => {
   const { data } = await axios.post(`${baseUrl}/api/signin?from=back`, {
     account,
     password,
--- src/pages/adminpage/AdminLoginPage.jsx
+++ src/pages/adminpage/AdminLoginPage.jsx
@@ -4,12 +4,14 @@ import { Link, useNavigate } from "react-router-dom";
 import styles from "pages/LoginRegister.module.scss";
 import { useEffect, useState } from "react";
 import { useAuth } from "context/authContext";
+import { adminLogin } from "api/admin";
 
 export const AdminLoginPage = () => {
   const [account, setAccount] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const { login, isAuthenticated } = useAuth();
+  // const { login, isAuthenticated } = useAuth();
+  const { isAuthenticated } = useAuth();
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -21,8 +23,10 @@ export const AdminLoginPage = () => {
     if (account.length === 0) return;
     if (password.length === 0) return;
     try {
-      const success = await login({ account, password });
-      if (success) {
+      const res = await adminLogin({ account, password });
+      if (res.token) {
+        localStorage.setItem("authToken", res.token);
+        localStorage.setItem("id", res.userData.id);
         alert("登入成功");
         navigate("/admin/tweetslist");
         return;
