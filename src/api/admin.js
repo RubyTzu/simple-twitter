a16be3e4e8337be56fb3b9705f9538f5fdfd08b9
Kaijun <kaijun_huang@outlook.com>
2023-06-13 10:51:04 +0800

    Feat: add admin user and tweets page, delete function

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,5 +1,6 @@
-// import axios from "axios";
-// const baseUrl = "https://twitter-2023.herokuapp.com";
+import axios from "axios";
+const baseUrl = "https://twitter-2023.herokuapp.com";
+const token = localStorage.getItem("authToken");
 
 // // export const adminLogin = async ({ account, password }) => {
 // //   const { data } = await axios.post(`${baseUrl}/api/signin`, {
@@ -15,13 +16,57 @@
 // //   }
 // // };
 
+export const login = async ({ account, password }) => {
+  const { data } = await axios.post(`${baseUrl}/api/signin?from=back`, {
+    account,
+    password,
+  });
+  const authToken = data.token;
+  console.log(data.token);
+  if (authToken) {
+    return { success: true, ...data };
+  } else {
+    return { success: false, ...data };
+  }
+};
 
-// export const getusers = async() => {
-// try{
-// const res = await axios.get(`${baseUrl}/api/admin/users`);
-// console.log(res);
+export const getTweets = async () => {
+  try {
+    const { data } = await axios.get(`${baseUrl}/api/admin/tweets`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// } catch(error){
-// console.error('[Get Users failed]: ', error);
-// }
-// }
+export const getUsers = async () => {
+  try {
+    const { data } = await axios.get(`${baseUrl}/api/admin/users`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const deleteTweet = async (id) => {
+  try {
+    const { data } = await axios.delete(`${baseUrl}/api/admin/tweets/${id}`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      param: {
+        id: id,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
