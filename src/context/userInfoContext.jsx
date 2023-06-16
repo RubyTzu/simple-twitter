f490c2ed455c8bd5b0bd6956d02485469104858f
Kaijun <kaijun_huang@outlook.com>
2023-06-16 23:32:45 +0800

    Refactor: adjust userInfoContext

--- src/context/userInfoContext.jsx
+++ src/context/userInfoContext.jsx
@@ -1,28 +1,17 @@
-import { useState, createContext, useContext, useEffect } from "react";
-// import * as jwt from "jsonwebtoken";
-import { getFollowCounts, getProfile } from "api/userinfo";
+import { useState, createContext, useContext } from "react";
 const CurrentUserContext = createContext();
 
 export const useCurrentUser = () => useContext(CurrentUserContext);
 
 export const CurrentUserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState({});
+  const [profile, setProfile] = useState({});
   const [followCounts, setFollowCounts] = useState({});
-  // const token = localStorage.getItem("authToken");
-  const id = localStorage.getItem("id");
-  // const { id } = jwt.decode(token);
-
-  useEffect(() => {
-    const getUserInfo = async () => {
-      setCurrentUser(await getProfile(id));
-      setFollowCounts(await getFollowCounts(id));
-    };
-    getUserInfo();
-  }, [id]);
 
   const value = {
-    currentUser: currentUser,
-    followNumber: followCounts,
+    profile,
+    setProfile,
+    followCounts,
+    setFollowCounts,
   };
 
   return (
